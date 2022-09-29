6c367b937f189be1313d16f3d630461183fc2a7f
Devon Carew <devoncarew@gmail.com>
2022-09-29 15:57:06 -0700

    updates

--- bin/firehose.dart
+++ bin/firehose.dart
@@ -1,4 +1,5 @@
 import 'package:firehose/firehose.dart' as firehose;
+import 'package:firehose/src/git.dart';
 
 // todo: support args (and help)
 
@@ -12,4 +13,10 @@ import 'package:firehose/firehose.dart' as firehose;
 
 void main(List<String> arguments) {
   print('Hello world: ${firehose.calculate()}!');
+
+  var git = Git();
+  print('git.baseRef: ${git.baseRef}');
+  print('git.headRef: ${git.headRef}');
+  print('git.ref: ${git.ref}');
+  print('git.refName: ${git.refName}');
 }
