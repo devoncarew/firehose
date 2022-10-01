c81f29d097f3f6b1cc8b1fc54564aa509cdfce91
Devon Carew <devoncarew@gmail.com>
2022-10-01 13:29:02 -0700

    publish implementation

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 0.2.0-dev.0
+
+- Added support for publishing packages from GitHub Actions.
+
 ## 0.1.0
 
 - Added support for verifying packages from PR branches.
--- lib/firehose.dart
+++ lib/firehose.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:firehose/src/packages.dart';
+import 'package:firehose/src/utils.dart';
 
 import 'src/git.dart';
 
@@ -85,6 +86,8 @@ class Firehose {
     }
   }
 
+  // todo: figure out how to share code between verify() and publish()
+
   void publish() {
 // for the default branch:
 // - determine changed files
@@ -92,10 +95,83 @@ class Firehose {
 // - validate that the pubspec version != a published version
 // - attempt to publish
 
-    // todo:
-    print('todo: publish');
+    var git = Git();
+
+    var changedFiles = git.getChangedFiles();
+    print('Repository changed files:');
+    for (var file in changedFiles) {
+      print('- $file');
+    }
+
+    var packages = Packages().locatePackages();
     print('');
-    verify();
+    print('Repository publishable packages:');
+    for (var package in packages) {
+      print('  $package');
+    }
+
+    var changedPackages = _calculateChangedPackages(packages, changedFiles);
+    print('');
+    print('Found ${changedPackages.length} changed package(s).');
+
+    for (var package in changedPackages) {
+      print('');
+      print('Publishing ${_bold('package:${package.pubspec.name}')}');
+
+      print('pubspec:');
+      print('  version: ${_bold(package.pubspec.version.toString())}');
+      if (package.pubspec.autoPublishValue != null) {
+        print('  auto_publish: ${package.pubspec.autoPublishValue}');
+      }
+      if (package.pubspec.publishToValue != null) {
+        print('  publish_to: ${package.pubspec.publishToValue}');
+      }
+
+      var files = package.matchingFiles(changedFiles);
+
+      print('changelog:');
+      var changelogUpdated = files.contains('CHANGELOG.md');
+      if (changelogUpdated) {
+        if (package.changelog.latestVersion != null) {
+          print('  ## ${_bold(package.changelog.latestVersion)}');
+        }
+        for (var entry in package.changelog.latestChangeEntries) {
+          print('  $entry');
+        }
+      }
+
+      print('changed files:');
+      for (var file in files) {
+        print('  $file');
+      }
+
+      // checks
+      if (!changelogUpdated) {
+        print('Note - no changelog update for this change.');
+      }
+      if (package.pubspec.version.toString() !=
+          package.changelog.latestVersion) {
+        print("Note - pubspec version (${package.pubspec.version}) and "
+            "changelog (${package.changelog.latestVersion})don't agree.");
+      }
+
+      // todo: use github secrets
+      var result = exec('dart', args: [
+        'pub',
+        'publish',
+        '--force',
+        '--directory=${package.directory.path}',
+      ]);
+      if (result.exitCode != 0) {
+        exitCode = result.exitCode;
+      }
+      if (result.stdout.isNotEmpty) {
+        print(result.stdout.trimRight());
+      }
+      if (result.stderr.isNotEmpty) {
+        stderr.writeln(result.stderr.trimRight());
+      }
+    }
   }
 
   List<Package> _calculateChangedPackages(
