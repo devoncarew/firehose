e3eb66011ae8cfda6226986144ed4fc45f0a9348
Devon Carew <devoncarew@gmail.com>
2023-01-23 13:07:01 -0800

    tweak output

--- lib/firehose.dart
+++ lib/firehose.dart
@@ -11,7 +11,7 @@ const String _dependabotUser = 'dependabot[bot]';
 
 const String _githubActionsUser = 'github-actions[bot]';
 
-const String _publishBotTag = '**publish bot**';
+const String _publishBotTag = '**publish action**';
 
 class Firehose {
   final Directory directory;
@@ -62,23 +62,21 @@ class Firehose {
       var repoTag = repo.calculateRepoTag(package);
 
       print('');
-      print('Validating ${_bold('package:${package.name}')}');
+      print('Validating $package:${package.name}');
 
       print('pubspec:');
       var pubspecVersion = package.pubspec.version;
-      print('  pubspec version: ${_bold(pubspecVersion)}');
+      print('  - version: $pubspecVersion');
 
       var changelogVersion = package.changelog.latestVersion;
       print('changelog:');
       print(package.changelog.describeLatestChanges.trimRight());
 
-      print('');
-
       if (pubspecVersion != changelogVersion) {
         var result = Result.fail(
           package,
           'pubspec version ($pubspecVersion) and changelog ($changelogVersion) '
-          "don't agree.",
+          "don't agree",
         );
         print(result);
         results.addResult(result);
@@ -87,14 +85,13 @@ class Firehose {
 
       if (await pub.hasPublishedVersion(package.name, pubspecVersion!)) {
         var result = Result.info(
-            package, '$pubspecVersion already published at pub.dev.');
+            package, '$pubspecVersion already published at pub.dev');
         print(result);
         results.addResult(result);
       } else if (package.pubspec.isPreRelease) {
         var result = Result.info(
           package,
-          'version ($pubspecVersion) is pre-release; no publish is '
-          'necessary.',
+          'version ($pubspecVersion) is pre-release; no publish is necessary',
         );
         print(result);
         results.addResult(result);
@@ -103,20 +100,14 @@ class Firehose {
             args: ['pub', 'publish', '--dry-run'], cwd: package.directory);
         if (code != 0) {
           exitCode = code;
-          results
-              .addResult(Result.fail(package, 'Pub publish dry-run failed.'));
+          results.addResult(Result.fail(package, 'pub publish dry-run failed'));
         } else {
-          var message =
-              'After merging, tag with $repoTag to trigger a publish.';
-          print('No issues found.\n$message');
+          print('No issues found.');
 
-          // "**publish bot**: package:firehose \@ 0.3.7 is ready to publish.
-          // After merging, tag with `v0.3.7` to trigger a publish.""
           var result = Result.success(
               package,
-              'package:${package.name} \\@ '
-              '${package.pubspec.version} is ready to publish. After merging, '
-              'tag with `$repoTag` to trigger a publish.');
+              '${package.pubspec.version} is ready to publish; after merging, '
+              'tagging with `$repoTag` will trigger publishing');
           print(result);
           results.addResult(result);
         }
@@ -190,12 +181,12 @@ class Firehose {
     }
 
     print('');
-    print('Publishing ${_bold('package:${package.name}')}');
+    print('Publishing ${'package:${package.name}'}');
     print('');
 
     print('pubspec:');
     var pubspecVersion = package.pubspec.version;
-    print('  version: ${_bold(pubspecVersion)}');
+    print('  version: $pubspecVersion');
 
     print('changelog:');
     print(package.changelog.describeLatestChanges);
@@ -224,10 +215,6 @@ class Firehose {
     return result == 0;
   }
 
-  String _bold(String? message) {
-    return '\u001b[1m$message\u001b[0m';
-  }
-
   Map<String, String> get env => Platform.environment;
 }
 
@@ -239,13 +226,9 @@ class VerificationResults {
   bool get hasSuccess => results.any((r) => r.severity == Severity.success);
 
   String get describe {
-    // var githubMessage = '$_publishBotTag: package:${package.name} \\@ '
-    //     '${package.pubspec.version} is ready to publish. After merging, '
-    //     'tag with `$repoTag` to trigger a publish.';
-
     return results.map((r) {
       var sev = r.severity == Severity.error ? '(error) ' : '';
-      return '- ${r.package.name}: $sev${r.message}';
+      return '- package:${r.package.name}: $sev${r.message}';
     }).join('\n');
   }
 }
